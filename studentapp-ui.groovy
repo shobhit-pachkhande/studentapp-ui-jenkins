ed72a602d168a6d27629a35fe34a66d40e76d94d
shobhit-pachkhande <155718267+shobhit-pachkhande@users.noreply.github.com>
2024-04-13 01:21:07 +0530

    --- Auto Git Commit ---

--- studentapp-ui.groovy
+++ studentapp-ui.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Pull') {
             steps {
-                git 'https://github.com/chetansomkuwar254/studentapp.ui.git'
+                git https://github.com/shobhit-pachkhande/studentapp.ui.git
                 echo 'Yes, Application repository pull is done !'
             }
         }
@@ -17,7 +17,7 @@ pipeline {
         stage('Test') {
             steps {
                sh '/opt/apache-maven-3.9.6/bin/mvn sonar:sonar   -Dsonar.projectKey=studentapp-ui   -Dsonar.host.url=http://13.234.204.176:9000   -Dsonar.login=5d71f02015694d38dad98d5a6d2641eff8adab2d'
-               
+
                 echo 'Here we are testing '
             }
         }
